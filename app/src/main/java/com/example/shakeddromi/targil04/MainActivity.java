2e3cdd384183f53963971d9abecff5f1f103c896
ShskedDromi <sd5308@bs.amalnet.k12.il>
2018-11-27 15:45:48 +0200

    Initial commit

---
+++ app/src/main/java/com/example/shakeddromi/targil04/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.shakeddromi.targil04;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    int x=1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=(Button) findViewById(R.id.button);
+    }
+
+    public void ClickNumber(View view) {
+            if ((x % 7 == 0) || (x % 10 == 7)) {
+                btn.setText("BOOM!");
+                x = x + 1;
+            }
+            else{
+                btn.setText("this is click number - "+x);
+                x = x + 1;
+            }
+        }
+    }
+
